298e01cbd3d9064c59f344965ee11d9d02ca7586
Stanislau Stasheuski <38816345+StasF1@users.noreply.github.com>
2019-03-26 02:19:10 +0300

    Add toInstall/ folder

---
+++ toIntstall/ubuntuSciencePackage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Personal Ubuntu package: deletes unneeded apps & installs needed
+# Before running add new theme to the termanal by copying the existing one
+
+echo "CHECK RUNNING BASH RIGHT FROM GITHUB"
+
+
+
+# *****************************************************************
